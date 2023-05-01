dc7aaa2e372ecfc4cb930f25505c378afbf0ac50
blakedennet2 <den18019@byui.edu>
2023-05-01 14:58:34 -0600

    added sprint script

--- Arena Game/Assets/StaminaMovement.cs
+++ Arena Game/Assets/StaminaMovement.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StaminaMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public Slider staminaBar;
+    private int maxStamina = 100;
+    private int currentStamina;
+
+    public static StaminaMovement instance;
+
+    private void Awake() {
+        instance = this;
+    }
+    
+    void start() {
+        currentStamina = maxStamina;
+        staminaBar.maxValue = maxStamina;
+        staminaBar.value = maxStamina;
+    }
+    public void UseStamina(int amount) {
+        if (currentStamina - amount >= 0) {
+            currentStamina -= amount;
+            staminaBar.value = currentStamina;
+        }
+        else {
+            Debug.Log("not enough stamina");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
